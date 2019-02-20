5ee5ea96c4bb09840946ae4aefb82ea415af9255
Daniel Halling <Daniel.Halling@emmet-software-labs.de>
2019-02-20 14:50:23 +0100

    Initial commit

---
+++ src/main/java/com/example/KickerStatistics/KickerStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.KickerStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickerStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickerStatisticsApplication.class, args);
+	}
+
+}
